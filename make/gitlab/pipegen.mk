0214b9e26ee74b345ac427e141426e8a355b8def
Stuart Ellis <stuart@stuartellis.name>
2023-04-10 11:31:06 +0100

    Try dynamic pipeline 6

--- make/gitlab/pipegen.mk
+++ make/gitlab/pipegen.mk
@@ -1,15 +1,27 @@
 
+## Constants
+
 PIPEGEN_TEMPLATE_ROOT_DIR := .gitlab/ci/templates
 PIPEGEN_TARGET_ROOT_DIR := tmp/pipegen
 
-PIPEGEN_TEMPLATE_SRC_PATH	:= $(PIPEGEN_TEMPLATE_ROOT_DIR)/check/terraform/.gitlab-ci.yml
-PIPEGEN_TARGET_OUTPUT_DIR	:= $(PIPEGEN_TARGET_ROOT_DIR)/check/terraform
+## User Options
+
+PIPEGEN_TASK 				?= dummy
+
+## Calculated variables
+
+PIPEGEN_TEMPLATE_SRC_PATH	:= $(PIPEGEN_TEMPLATE_ROOT_DIR)/$(PIPEGEN_TASK)/.gitlab-ci.yml
+PIPEGEN_TARGET_OUTPUT_DIR	:= $(PIPEGEN_TARGET_ROOT_DIR)/$(PIPEGEN_TASK)
 
 ## Targets ##
 
+.PHONY: pipegen-clean
+pipegen-clean:
+	@rm -r $(PIPEGEN_TARGET_ROOT_DIR)
+
 .PHONY: pipegen-get-ytt
 pipegen-get-ytt:
-	apk add ytt --repository=https://dl-cdn.alpinelinux.org/alpine/edge/testing
+	@apk add ytt --repository=https://dl-cdn.alpinelinux.org/alpine/edge/testing
 
 .PHONY: pipegen-build
 pipegen-build:
