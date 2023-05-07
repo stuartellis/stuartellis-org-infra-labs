8bef3eed76c18ae2b2c8efa90b10865b8c52ae43
Stuart Ellis <stuart@stuartellis.name>
2023-05-07 15:45:32 +0100

    feat: Use env vars for backend config

--- make/terraform/terraform-cli.mk
+++ make/terraform/terraform-cli.mk
@@ -15,15 +15,16 @@ TF_ROOT           := $(ST_STACKS_DEFS_DIR)/$(STACK_NAME)
 TF_PLAN_CACHE     := $(TF_ROOT)/plan.cache
 TF_PLAN_JSON      := $(TF_ROOT)/plan.json
 TF_STATE_NAME     := $(PRODUCT_NAME)-$(ENVIRONMENT)-$(STACK_NAME)-$(STACK_VARIANT)
-TF_VARS_OPT       := -var="product_name=$(PRODUCT_NAME)" -var="stack_name=$(STACK_NAME)" -var="environment=$(ENVIRONMENT)" -var="variant=$(STACK_VARIANT)"
-TF_VAR_FILES_OPT  := -var-file=$(ST_STACKS_ENVS_DIR)/all/$(STACK_NAME).tfvars -var-file=$(ST_STACKS_ENVS_DIR)/$(ENVIRONMENT)/$(STACK_NAME).tfvars
+
+ST_VARS_OPT       := -var="product_name=$(PRODUCT_NAME)" -var="stack_name=$(STACK_NAME)" -var="environment=$(ENVIRONMENT)" -var="variant=$(STACK_VARIANT)"
+ST_VAR_FILES_OPT  := -var-file=$(ST_STACKS_ENVS_DIR)/all/$(STACK_NAME).tfvars -var-file=$(ST_STACKS_ENVS_DIR)/$(ENVIRONMENT)/$(STACK_NAME).tfvars
 
 # Terraform plan
-TF_PLAN_FILE      := $(TF_STATE_NAME).tfplan
-TF_PLAN_PATH      := $(ST_TF_TMP_DIR)/$(TF_PLAN_FILE)
-TF_PLAN_FILE_OPT  := -out=$(TF_PLAN_PATH)
+ST_PLAN_FILE      := $(TF_STATE_NAME).tfplan
+ST_PLAN_PATH      := $(ST_TF_TMP_DIR)/$(ST_PLAN_FILE)
+ST_PLAN_FILE_OPT  := -out=$(ST_PLAN_PATH)
 
-TF_CHDIR_OPT        := -chdir=$(TF_ROOT)
+ST_CHDIR_OPT        := -chdir=$(TF_ROOT)
 
 ##  Variables for GitLab ##
 # TF_IN_AUTOMATION 	:= true
@@ -41,6 +42,16 @@ ifdef CI
 	TF_HTTP_USERNAME		:= $(GL_TF_USERNAME)
 	TF_HTTP_PASSWORD		:= $(GL_TF_PASSWORD)
 	TF_HTTP_RETRY_WAIT_MIN	:= 5
+
+	ST_BACKEND_ENV_VARS		:= 	TF_HTTP_ADDRESS=$(TF_HTTP_ADDRESS) \
+		TF_HTTP_LOCK_ADDRESS=$(TF_HTTP_LOCK_ADDRESS) \
+		TF_HTTP_LOCK_METHOD=$(TF_HTTP_LOCK_METHOD) \
+		TF_HTTP_UNLOCK_ADDRESS=$(TF_HTTP_UNLOCK_ADDRESS) \
+		TF_HTTP_UNLOCK_METHOD=$(TF_HTTP_UNLOCK_METHOD) \
+		TF_HTTP_USERNAME=$(TF_HTTP_USERNAME) \
+		TF_HTTP_PASSWORD=$(TF_HTTP_PASSWORD) \
+		TF_HTTP_RETRY_WAIT_MIN=$(TF_HTTP_RETRY_WAIT_MIN) \
+
 else
 	TF_RUN_CMD_OPTS   := -backend=false
 endif
@@ -49,39 +60,23 @@ endif
 
 .PHONY: terraform-apply
 terraform-apply:
-	@terraform $(TF_CHDIR_OPT) apply -auto-approve $(TF_PLAN_PATH)
+	$(ST_BACKEND_ENV_VARS) @terraform $(ST_CHDIR_OPT) apply -auto-approve $(ST_PLAN_PATH)
 
 .PHONY: terraform-check-fmt
 terraform-check-fmt:
-	@terraform $(TF_CHDIR_OPT) fmt -check -diff -recursive
+	@terraform $(ST_CHDIR_OPT) fmt -check -diff -recursive
 
 .PHONY: terraform-fmt
 terraform-fmt:
-	@terraform $(TF_CHDIR_OPT) fmt
+	@terraform $(ST_CHDIR_OPT) fmt
 
 .PHONY: terraform-init
 terraform-init:
-	TF_HTTP_ADDRESS=$(TF_HTTP_ADDRESS) \
-	TF_HTTP_LOCK_ADDRESS=$(TF_HTTP_LOCK_ADDRESS) \
-	TF_HTTP_LOCK_METHOD=$(TF_HTTP_LOCK_METHOD) \
-	TF_HTTP_UNLOCK_ADDRESS=$(TF_HTTP_UNLOCK_ADDRESS) \
-	TF_HTTP_UNLOCK_METHOD=$(TF_HTTP_UNLOCK_METHOD) \
-	TF_HTTP_USERNAME=$(TF_HTTP_USERNAME) \
-	TF_HTTP_PASSWORD=$(TF_HTTP_PASSWORD) \
-	TF_HTTP_RETRY_WAIT_MIN=$(TF_HTTP_RETRY_WAIT_MIN) \
-	terraform $(TF_CHDIR_OPT) init $(TF_VARS_OPT)
+	$(ST_BACKEND_ENV_VARS) terraform $(ST_CHDIR_OPT) init $(ST_VARS_OPT)
 
 .PHONY: terraform-plan
 terraform-plan:
-	TF_HTTP_ADDRESS=$(TF_HTTP_ADDRESS) \
-	TF_HTTP_LOCK_ADDRESS=$(TF_HTTP_LOCK_ADDRESS) \
-	TF_HTTP_LOCK_METHOD=$(TF_HTTP_LOCK_METHOD) \
-	TF_HTTP_UNLOCK_ADDRESS=$(TF_HTTP_UNLOCK_ADDRESS) \
-	TF_HTTP_UNLOCK_METHOD=$(TF_HTTP_UNLOCK_METHOD) \
-	TF_HTTP_USERNAME=$(TF_HTTP_USERNAME) \
-	TF_HTTP_PASSWORD=$(TF_HTTP_PASSWORD) \
-	TF_HTTP_RETRY_WAIT_MIN=$(TF_HTTP_RETRY_WAIT_MIN) \
-	terraform $(TF_CHDIR_OPT) plan $(TF_PLAN_FILE_OPT) $(TF_VARS_OPT) $(TF_VAR_FILES_OPT)
+	$(ST_BACKEND_ENV_VARS) terraform $(ST_CHDIR_OPT) plan $(ST_PLAN_FILE_OPT) $(ST_VARS_OPT) $(ST_VAR_FILES_OPT)
 
 .PHONY: terraform-install
 terraform-install:
