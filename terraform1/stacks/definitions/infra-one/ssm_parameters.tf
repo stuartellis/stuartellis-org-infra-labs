4c7595be29cd7ded5e659c934cc6aefd8882ce09
Stuart Ellis <stuart@stuartellis.name>
2023-05-07 16:10:11 +0100

    fix: Add script for task

--- terraform1/stacks/definitions/infra-one/ssm_parameters.tf
+++ terraform1/stacks/definitions/infra-one/ssm_parameters.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "stack_present" {
-  name  = "/stacks/${var.product_name}/${var.stack_name}/${var.environment}/${var.variant}"
+  name  = "/stacks/${var.product_name}/${var.stack_name}/${var.environment}/${var.variant}/present"
   type  = "String"
   value = "true"
 }
