69f8b00774f2ef2ae6891f299db3a0650bd5dad4
Stuart Ellis <stuart@stuartellis.name>
2023-05-08 10:59:06 +0100

    feat: Output TF plan

--- make/terraform/terraform-cli.mk
+++ make/terraform/terraform-cli.mk
@@ -84,7 +84,7 @@ terraform-plan:
 	mkdir -p $(ST_TF_TMP_DIR)
 	$(ST_BACKEND_ENV_VARS) terraform $(ST_CHDIR_OPT) plan $(ST_PLAN_FILE_OPT) $(ST_VARS_OPT) $(ST_VAR_FILES_OPT)
 
-.PHONY: terraform-show
+.PHONY: terraform-show-json
 terraform-show:
 	terraform show --json $(ST_PLAN_PATH) | convert_report > $(ST_TF_PLAN_JSON)
 
