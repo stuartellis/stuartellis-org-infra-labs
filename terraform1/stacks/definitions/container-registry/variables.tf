87de699be3dcd311e94dca055e23e133e669ba32
Stuart Ellis <stuart@stuartellis.name>
2023-05-08 21:11:16 +0100

    feat: Add container-registry stack

---
+++ terraform1/stacks/definitions/container-registry/variables.tf
@@ -0,0 +1,19 @@
+variable "environment" {
+  type = string
+}
+
+variable "product_name" {
+  type = string
+}
+
+variable "stack_name" {
+  type = string
+}
+
+variable "variant" {
+  type = string
+}
+
+variable "tf_exec_role_arn" {
+  type = string
+}
