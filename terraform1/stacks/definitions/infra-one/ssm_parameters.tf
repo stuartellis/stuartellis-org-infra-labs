27e38ad15176b64f1a542a654f40e3ab7701e6fd
Stuart Ellis <stuart@stuartellis.name>
2023-06-05 23:55:30 +0100

    chore: pipeline 20

--- terraform1/stacks/definitions/infra-one/ssm_parameters.tf
+++ terraform1/stacks/definitions/infra-one/ssm_parameters.tf
@@ -3,3 +3,9 @@ resource "aws_ssm_parameter" "stack_present" {
   type  = "String"
   value = "true"
 }
+
+resource "aws_ssm_parameter" "stack_param_one" {
+  name  = "/stacks/${var.environment}/${var.product_name}/${var.stack_name}/${var.variant}/one"
+  type  = "String"
+  value = "One"
+}
