8e2d867da1e5e7d7da4de6bbf32614703615fda9
Stuart Ellis <stuart@stuartellis.name>
2024-05-05 17:37:43 +0100

    feat: Amend parameters name

--- tf/definitions/container-registry/ssm_parameters.tf
+++ tf/definitions/container-registry/ssm_parameters.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "stack_present" {
-  name  = "/stacks/${var.environment}/${var.product_name}/${var.stack_name}/${var.variant}/present"
+  name  = "/metadata/${var.environment_name}/${var.product_name}/${var.stack_name}/${var.variant}/present"
   type  = "String"
   value = "true"
 }
