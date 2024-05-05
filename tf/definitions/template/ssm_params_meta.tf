6d106b1d4a4504b3acd958b790fb0b883cb92114
Stuart Ellis <stuart@stuartellis.name>
2024-05-05 23:27:28 +0100

    fix: Remove obsolete template

--- tf/definitions/template/ssm_params_meta.tf
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2024-present Stuart Ellis <stuart@stuartellis.name>
-#
-# SPDX-License-Identifier: MIT
-
-resource "aws_ssm_parameter" "stack_present" {
-  name  = "/metadata/${var.product_name}/${var.stack_name}/${var.environment_name}/${var.variant}/present"
-  type  = "String"
-  value = "true"
-}
