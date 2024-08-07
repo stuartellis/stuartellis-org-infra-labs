190277cf5df472038bcd405623c48c23e559dd1f
Stuart Ellis <stuart@stuartellis.name>
2024-08-07 21:56:36 +0100

    fix: Remove compute-coracle

--- tf/definitions/compute-coracle/locals.tf
+++
@@ -1,7 +0,0 @@
-# SPDX-FileCopyrightText: 2024-present Stuart Ellis <stuart@stuartellis.name>
-#
-# SPDX-License-Identifier: MIT
-
-locals {
-  ec2_prefix = "${var.product_name}-${var.environment_name}-${var.stack_name}-${var.variant}"
-}
