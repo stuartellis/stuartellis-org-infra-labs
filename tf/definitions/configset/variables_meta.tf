c41e98972f7cb4167654aaa572bc78172344abf4
Stuart Ellis <stuart@stuartellis.name>
2024-08-10 08:48:19 +0100

    feat: Add stack for configset

---
+++ tf/definitions/configset/variables_meta.tf
@@ -0,0 +1,23 @@
+# SPDX-FileCopyrightText: 2024-present Stuart Ellis <stuart@stuartellis.name>
+#
+# SPDX-License-Identifier: MIT
+
+variable "environment_name" {
+  type = string
+}
+
+variable "product_name" {
+  type = string
+}
+
+variable "stack_name" {
+  type = string
+}
+
+variable "variant" {
+  type = string
+}
+
+variable "tf_exec_role_arn" {
+  type = string
+}
